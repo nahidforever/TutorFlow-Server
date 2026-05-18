50dd6170ddde7faa158cd2da81f1efa36a1c69ce
Nahid1828 <isam15-5334@diu.edu.bd>
2026-05-18 17:20:16 +0600

    Create 6 tutor limit get api and single tutor get API

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = process.env.MONGO_DB_URL;
 
 app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -24,8 +25,22 @@ async function run() {
     const db = client.db("tutorflow-db");
     const tutorCollection = db.collection("tutor");
 
+    app.get("/available-tutor", async (req, res) => {
+      const result = await tutorCollection.find().limit(6).toArray();
+      res.send(result);
+    });
+
     app.get("/tutor", async (req, res) => {
       const result = await tutorCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/tutor/:id", async (req, res) => {
+      const { id } = req.params;
+
+      const result = await tutorCollection.findOne({
+        _id: new ObjectId(id),
+      });
       res.send(result);
     });
 
